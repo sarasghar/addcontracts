dff366e01ea6b4091417244c7fd41843c3e16928
Sara Asghar <sarasghar@gmail.com>
2020-06-25 11:49:25 +0200

    yarn installed and logo removed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import AddContract from "./AddContract/AddContract.js";
 import "bootstrap/dist/css/bootstrap.min.css";
